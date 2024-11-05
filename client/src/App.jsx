f79476fc7aeaba79a3750e68d4f20b34b31d8382
ravikr126 <ravi14577@gmail.com>
2024-11-05 23:04:14 +0530

    tailwind , frontend and backend setup done

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button className="bg-red-400">Click me</Button>
+    </>
+  );
+}
+
+export default App;
