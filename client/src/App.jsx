47c7ed6f9c9ea0d193a7f11bff341aa034ecf6e2
ravikr126 <ravi14577@gmail.com>
2024-11-05 23:27:14 +0530

    tailwind issue solve

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import { Button } from "./components/ui/button";
 function App() {
   return (
     <>
-      <Button className="bg-red-400">Click me</Button>
+      <Button className="bg-red-400 text-3xl">Click me</Button>
     </>
   );
 }
